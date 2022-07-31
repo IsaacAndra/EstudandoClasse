9bbd6d65fd70d12c9164b6619dca9a5c51ec7ce9
IsaacAndra <isaac.andra84@gmail.com>
2022-07-31 14:56:20 -0300

    Criando Um Método e Mudando Seus Parâmetros

--- Assets/Ogro.cs
+++ Assets/Ogro.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Ogro : MonoBehaviour
 {
+    //Pensando nos atributos do Ogro
+    //Os atributos de uma Classe, são as suas variáveis.
+
+    //Vida, velocidade, nome próprio.
+    public float vida = 10f;
+    public float velocidade = 5f;
+    public string nome = "Josué";
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Fazendo o ogro se apresentar
+        Apresentar(nome);
+
+        Apresentar("Ana");
+
+        Apresentar(vida.ToString());
+
+
     }
 
     // Update is called once per frame
@@ -15,4 +31,17 @@ public class Ogro : MonoBehaviour
     {
         
     }
+
+    //Criando o Método de apresentação
+    public void Apresentar(string nome)
+    {
+        //Esse código é o meu método
+        Debug.Log("Prazer meu nome é " + nome);
+
+        //Usano o atributo ao invés do parâmetro
+        Debug.Log("Meu atributo é " + this.nome);
+    }
+
+
+
 }
